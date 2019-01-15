240e1df6bfa6761733fad73b300a635aa3f9ee96
volker <v.weckbach@hs-mannheim.de>
2019-01-15 14:19:58 +0100

    cblocks stubs export and mongo volume

--- src/test/stubs/cblocks/index.js
+++ src/test/stubs/cblocks/index.js
@@ -1,11 +1,15 @@
 exports.temperature = require('./temperature.js');
 exports.led = require('./led.js');
+exports.rfid = require('./rfid.js');
+exports.scale = require('./scale.js');
 
 exports.all = (() => {
   let result = [];
 
   result.push(exports.temperature);
   result.push(exports.led);
+  result.push(exports.rfid);
+  result.push(exports.scale);
 
   return result;
 })();
