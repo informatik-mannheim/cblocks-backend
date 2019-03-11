114a19bb00cca855c284850a490486e9b7b067aa
volker <v.weckbach@hs-mannheim.de>
2019-03-11 09:40:25 +0100

    vibration motor stubs

---
+++ src/test/stubs/cblocks/vibration.js
@@ -0,0 +1,22 @@
+module.exports = {
+  'objectID': 3307,
+  'name': 'LED Stripe',
+  'resources': {
+    '0': {
+      'resourceID': 0,
+      'name': 'Percentage',
+      'is_writeable': true,
+      'schema': {
+        'type': 'number',
+        'minimum': 0,
+        'maximum': 100,
+        'additionalProperties': false,
+      },
+    },
+  },
+  'instances': {
+    '0': {
+      'label': 'Vibration Motor',
+    },
+  },
+};
---
+++ src/test/stubs/labelMappings/vibrationLabelMapping.js
@@ -0,0 +1,18 @@
+module.exports = {
+  'mappingID': 4711,
+  'label': 'Vibration Label Mapping',
+  'objectID': 3307,
+  'resourceID': 0,
+  'instanceID': 0,
+  'default': 'Undefined',
+  'labels': [
+    {
+      'value': 0,
+      'label': 'Off',
+    },
+    {
+      'value': 100,
+      'label': 'On',
+    },
+  ],
+};
